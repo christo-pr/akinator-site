447d6f5b3ede300b25bb0c442965bec1215b40b6
Christofer Flores <cristofer.flort@gmail.com>
2020-05-01 14:29:30 -0500

    Install ness.css and create a basic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import { Header } from "../components"
+
+export default () => {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="activate-camera-container">
+          <div className="message">
+            <i className="nes-bcrikko"></i>
+            <div className="nes-balloon from-left">
+              <p className="text-left">
+                Welcome to Akin-site! You can think on a character and I'll try
+                to guess it by asking some questions!
+                <br />
+                To start you need to enable camera permissions:
+              </p>
+              <button type="button" className="nes-btn is-primary">
+                Enable camera
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
